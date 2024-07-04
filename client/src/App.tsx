3bed7da777142a6e24fe6088cb011742345bc3ca
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-07-04 11:32:17 +0100

    Client Setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <>
+      <h2>Dia duit as Chakra UI</h2>
+      <Button>Haigh</Button>
+    </>
+  );
+}
+
+export default App;
