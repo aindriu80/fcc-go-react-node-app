1e9e175c5e2f1b54a334c461d3c2bd797b6e950f
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-07-06 16:47:41 +0100

    Todo form - initial

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import { Container, Stack } from '@chakra-ui/react';
-import Navbar from './Components/Navbar';
+import Navbar from './components/Navbar';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
     <Stack h="100vh">
       <Navbar />
       <Container>
-        {/* <TodoForm/> */}
+        <TodoForm />
         {/* <TodoList/> */}
       </Container>
     </Stack>
